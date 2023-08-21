81f005fd43129b264162ab733d11c8a83562c2a7
logicipher <zhanpeng@DESKTOP-TOOOT0Q>
2023-08-21 19:12:27 +0800

    feat:add initial doc

---
+++ src/list_mutex.cpp
@@ -0,0 +1,9 @@
+/*
+ * This file contains an implementation of parallel list structure
+ * using mutex.
+ */
+
+namespace mutex_struct {
+
+template <typename ElemType> class list_mutex {};
+} // namespace mutex_struct
